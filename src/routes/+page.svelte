37e525d8006be75d7277a97e37a6778c10f8cad0
unknown <admin@zFu9crAQU7.zFu9crAQU7>
2024-05-13 22:46:06 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="section" id="intro">
+      <h1>GAME<span>STUMP</span></h1>
+      <p>The ultimate Solana GME meme $GEME</p>
+    </div>
+
+    <div class="section" id="links">
+        <h1>$GEME Links</h1>
+        <div class="social-links">
+            <a href="https://twitter.com/GameStumpSol" target="_blank" rel="noopener" class="social-link">
+                <img src="xlogo.webp" alt="Twitter"/>
+            </a>
+            <a href="https://dexscreener.com/solana/5224rcugggwgm7y3ffhrnfz16dk72zmpjx31btv4bcpi" target="_blank" rel="noopener" class="social-link">
+                <img src="dexscreener.webp" alt="DexScreener"/>
+            </a>
+            <a href="https://t.me/GAMESTUMP" target="_blank" rel="noopener" class="social-link">
+                <img src="telegramlogo.webp" alt="Telegram"/>
+            </a>
+        </div>
+      </div>
+
+    <div class="section" id="about">
+        <h1>About</h1>
+        <p>$GEME is a strong community-backed project with a team willing to spend their bag for your success. With an onboarded artist and a strong team there is nothing better on Solana than $GEME</p>
+      </div>
+
+      <div class="section" id="tokenomics">
+        <h1>Tokenomics</h1>
+        <img src="tokenomics.webp" alt="tokenomics"/>
+      </div>
+
+      <div class="section" id="credits">
+        <a href="https://t.me/fatlzt" target="_blank" rel="noopener">
+            <p>Credits @fatlzt on Telegram for Site</p>
+        </a>
+    </div>    
+</div>
+
+<style>
+    @import './styles/styles.css';
+</style>
